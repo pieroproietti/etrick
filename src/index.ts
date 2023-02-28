bf7f2eb811f60a880d806a614d231ed86e730364
Piero Proietti <piero.proietti@gmail.com>
2023-02-28 19:38:09 +0100

    again don't work... atch!!!

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@
  */
 export const MessageTypes = require('./lib/packet/message-types').default
 export const Options = require('./lib/packet/options').default
-export const etrick = require('./classes/etrick')
+import etrick from './classes/etrick'
+export default etrick 
